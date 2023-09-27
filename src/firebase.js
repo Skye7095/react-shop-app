eae9cef8615ce9eec5caeed27d7c65893eb9f117
Skye <hkim7095@gmail.com>
2023-09-27 23:39:07 +0900

    signup + login

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkwDJdzNSyf5h3mbdf_CtUYijhEuf8FzQ",
+  authDomain: "react-shop-app-2a9f5.firebaseapp.com",
+  projectId: "react-shop-app-2a9f5",
+  storageBucket: "react-shop-app-2a9f5.appspot.com",
+  messagingSenderId: "366751326309",
+  appId: "1:366751326309:web:a89a45d917f23f684ba68c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/LoginPage/sign-in/SignIn.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Form from '../../../components/form/Form'
+import { useNavigate } from 'react-router-dom'
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth'
+import app from '../../../firebase'
+
+const SignIn = () => {
+
+  const navigate = useNavigate()
+  const [firebaseError, setFirebaseError] = useState("")
+
+  const auth = getAuth(app)
+  const handleLogin = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(user => {
+        navigate('/')
+      })
+      .catch(error => {
+        return error && setFirebaseError("이메일 또는 비밀번호가 잘못되었습니다.")
+      })
+  }
+
+  return (
+    <Form
+      title='로그인'
+      getDataForm={handleLogin}
+      firebaseError={firebaseError}
+    />
+  )
+}
+
+export default SignIn
