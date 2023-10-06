979f677581c1c3d63cd8195b4063557ca7e49297
Skye <hkim7095@gmail.com>
2023-10-06 23:04:01 +0900

    장바구니 UI part1

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import CartEmpty from '../../components/cart-empty/CartEmpty'
+import CartList from './cart-list/CartList'
+import Checkout from './checkout/Checkout'
 
 const CartPage = () => {
+
+  const {products} = useSelector((state) => state.cartSlice)
+
   return (
-    <div>CartPage</div>
+    <div className='page'>
+      {!products.length ? (
+        <CartEmpty title={title} />
+      ) : (
+        <div className='container'>
+          <h1>장바구니</h1>
+          <CartList />
+          <Checkout />
+        </div>  
+      )}
+    </div>
   )
 }
 
